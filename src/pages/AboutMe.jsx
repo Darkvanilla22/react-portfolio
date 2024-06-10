d2f617e360be1ad03a5b2c9f7cbc2bd3c285d14e
Finian Corbett <finian97@gmail.com>
2024-06-10 18:42:08 -0400

    Built webapp for netlify deployment + added profile picture to 'About Me'

--- src/pages/AboutMe.jsx
+++ src/pages/AboutMe.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import '../assets/styles/AboutMe.css';
+import profilePic from '../assets/images/profile.jpg';
 
 const AboutMe = () => (
-  <section>
+  <section className="about-me">
     <h2>About Me</h2>
-    <img src="path/to/photo.jpg" alt="Developer" />
+    <img src={profilePic} alt="Developer" className="profile-pic" />
     <p>Short bio about the developer.</p>
   </section>
 );
