93614101dcadc6b78d35c801d8cb4fba0db8429a
Finian Corbett <finian97@gmail.com>
2024-06-10 19:19:09 -0400

    Added 2nd & 3rd project to Portfolio

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import '../assets/styles/Portfolio.css';
 import Project from '../components/Project';
 import pwaTextEditor from '../assets/images/pwa-text-editor.png';
+import weatherReport from '../assets/images/weather-report-dashboard.png';
+import expressNoteTaker from '../assets/images/express-note-taker.png';
 
 const Portfolio = () => {
   const projects = [
@@ -10,6 +12,16 @@ const Portfolio = () => {
       deployedLink: 'https://pwa-text-editor-g8vs.onrender.com/', 
       repoLink: 'https://github.com/Darkvanilla22/PWA-Text-Editor' },
     // Add more projects here
+    { title: 'Weather Report Dashboard', 
+      image: weatherReport, 
+      deployedLink: 'https://darkvanilla22.github.io/weather-report/', 
+      repoLink: 'https://github.com/Darkvanilla22/weather-report' },
+    // Add more projects here
+    { title: 'Express Note Taker',
+      image: expressNoteTaker,
+      deployedLink: 'https://express-note-taker-fadda8ca939d.herokuapp.com/',
+      repoLink: 'https://github.com/Darkvanilla22/express-note-taker'
+    }
   ];
 
   return (
