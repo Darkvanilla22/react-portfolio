e426a806b01fec8ade18a8f101ebe0e7f64cecd1
Finian Corbett <finian97@gmail.com>
2024-06-10 20:10:42 -0400

    Added styling and proficiencies to the 'Resume' page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,15 +2,18 @@ import React from 'react';
 import '../assets/styles/Resume.css';
 
 const Resume = () => (
-  <section>
+  <section className="resume">
     <h2>Resume</h2>
     <a href="path/to/resume.pdf" download>Download Resume</a>
     <h3>Proficiencies</h3>
     <ul>
-      <li>Proficiency 1</li>
-      <li>Proficiency 2</li>
-      <li>Proficiency 3</li>
-      {/* Add more proficiencies here */}
+      <li>HTML</li>
+      <li>CSS</li>
+      <li>JavaScript</li>
+      <li>React</li>
+      <li>Node.js</li>
+      <li>Express</li>
+      <li>MongoDB</li>
     </ul>
   </section>
 );
