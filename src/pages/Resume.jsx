b52b3141ee452b84770de7565cd80b8d65974104
Finian Corbett <finian97@gmail.com>
2024-06-10 20:13:34 -0400

    Centered content for the Resume Page

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,20 +2,22 @@ import React from 'react';
 import '../assets/styles/Resume.css';
 
 const Resume = () => (
-  <section className="resume">
-    <h2>Resume</h2>
-    <a href="path/to/resume.pdf" download>Download Resume</a>
-    <h3>Proficiencies</h3>
-    <ul>
-      <li>HTML</li>
-      <li>CSS</li>
-      <li>JavaScript</li>
-      <li>React</li>
-      <li>Node.js</li>
-      <li>Express</li>
-      <li>MongoDB</li>
-    </ul>
-  </section>
+  <div className="resume-container">
+    <section className="resume">
+      <h2>Resume</h2>
+      <a href="path/to/resume.pdf" download>Download Resume</a>
+      <h3>Proficiencies</h3>
+      <ul>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>JavaScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Express</li>
+        <li>MongoDB</li>
+      </ul>
+    </section>
+  </div>
 );
 
 export default Resume;
