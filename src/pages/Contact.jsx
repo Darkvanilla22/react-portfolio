0a9105226ce8a3d8eefecd2d5a81f3b9bddf8954
Finian Corbett <finian97@gmail.com>
2024-06-10 21:50:49 -0400

    Centered content for 'Contact' page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -25,44 +25,49 @@ const Contact = () => {
   };
 
   return (
-    <section>
-      <h2>Contact</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label>Name:</label>
-          <input
-            type="text"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {errors.name && <span>{errors.name}</span>}
-        </div>
-        <div>
-          <label>Email:</label>
-          <input
-            type="email"
-            name="email"
-            value={formData.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          />
-          {errors.email && <span>{errors.email}</span>}
-        </div>
-        <div>
-          <label>Message:</label>
-          <textarea
-            name="message"
-            value={formData.message}
-            onChange={handleChange}
-            onBlur={handleBlur}
-          ></textarea>
-          {errors.message && <span>{errors.message}</span>}
-        </div>
-        <button type="submit">Submit</button>
-      </form>
-    </section>
+    <div className="contact-container">
+      <section className="contact">
+        <h2>Contact</h2>
+        <form onSubmit={handleSubmit}>
+          <div>
+            <label htmlFor="name">Name:</label>
+            <input
+              type="text"
+              id="name"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.name && <span>{errors.name}</span>}
+          </div>
+          <div>
+            <label htmlFor="email">Email:</label>
+            <input
+              type="email"
+              id="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            />
+            {errors.email && <span>{errors.email}</span>}
+          </div>
+          <div>
+            <label htmlFor="message">Message:</label>
+            <textarea
+              id="message"
+              name="message"
+              value={formData.message}
+              onChange={handleChange}
+              onBlur={handleBlur}
+            ></textarea>
+            {errors.message && <span>{errors.message}</span>}
+          </div>
+          <button type="submit">Submit</button>
+        </form>
+      </section>
+    </div>
   );
 };
 
